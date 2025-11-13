72504d900d8a24a0134336e6d8240d78546d1e2b
killerrechird <zhouzl_2002@163.com>
2025-11-13 17:27:05 +0800

    完善细节

--- Client/CodeBaseBuild/build_codebase.py
+++ Client/CodeBaseBuild/build_codebase.py
@@ -129,12 +129,3 @@ def get_repository(repo_url, destination='./repos'):
     Repo.clone_from(repo_url, destination)
     print('Repository cloned to', destination)
     return True
-
-def main():
-
-    repo_url = 'git@github.com:lyusupov/SoftRF.git'
-    print(get_repository(repo_url, destination='./repos/SoftRF'))
-
-
-if __name__ == '__main__':
-    main()
